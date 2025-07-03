9a783785e0d638c656ddab0e811a1c58667dcdf3
MenduVamsi <goldyvams@gmail.com>
2025-07-04 01:14:58 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,12 +1,1 @@
-print("hello world")
-# Simple addition of two numbers
 
-# Input from user
-num1 = "10.5"
-num2 = (input("Enter second number: "))
-
-# Add the numbers
-sum = num1 + num2
-
-# Display the result
-print("The sum is:", sum)
